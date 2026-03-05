9fe09620778318342420be1ed651b4eb6f81cf0d
Emiliano txt <108497199+labtxt@users.noreply.github.com>
2026-03-05 18:22:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,76 @@
-// SCROLL SUAVE
+/* SCROLL SUAVE */
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
 
 anchor.addEventListener('click',function(e){
 
-e.preventDefault()
+e.preventDefault();
 
-const target=document.querySelector(this.getAttribute('href'))
+const target=document.querySelector(this.getAttribute('href'));
+
+if(target){
 
 window.scrollTo({
-
 top:target.offsetTop-80,
-
 behavior:'smooth'
+});
 
-})
+}
 
-})
+});
 
-})
+});
 
 
-// MODAL
+/* MODAL */
 
 function openModal(img){
 
-document.getElementById("modalImage").src=img.src
+document.getElementById("modalImage").src=img.src;
 
-document.getElementById("imageModal").style.display="flex"
+document.getElementById("imageModal").style.display="flex";
+
+document.body.style.overflow="hidden";
 
 }
 
 function closeModal(){
 
-document.getElementById("imageModal").style.display="none"
+document.getElementById("imageModal").style.display="none";
+
+document.body.style.overflow="auto";
 
 }
 
 
-// TEMA OSCURO / CLARO
+/* TEMA */
 
-const toggle=document.getElementById("themeToggle")
+const themeBtn=document.getElementById("themeToggle");
 
 if(localStorage.getItem("theme")==="light"){
 
-document.body.classList.add("light-mode")
+document.body.classList.add("light-mode");
 
-toggle.textContent="☀️"
+themeBtn.textContent="☀️";
 
 }
 
-toggle.addEventListener("click",()=>{
+themeBtn.addEventListener("click",()=>{
 
-document.body.classList.toggle("light-mode")
+document.body.classList.toggle("light-mode");
 
 if(document.body.classList.contains("light-mode")){
 
-toggle.textContent="☀️"
+themeBtn.textContent="☀️";
 
-localStorage.setItem("theme","light")
+localStorage.setItem("theme","light");
 
 }else{
 
-toggle.textContent="🌙"
+themeBtn.textContent="🌙";
 
-localStorage.setItem("theme","dark")
+localStorage.setItem("theme","dark");
 
 }
 
-})
+});
